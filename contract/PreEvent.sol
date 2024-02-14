d793ddfed94ab64ebd7704d20e9ed2f6fe01da64
Sepang2 <sepang296@gmail.com>
2024-02-14 16:52:18 +0900

    Change abi, address of new test-contract

--- contract/PreEvent.sol
+++ contract/PreEvent.sol
@@ -35,7 +35,7 @@ contract PreTicket is ERC721Enumerable, Ownable {
     }
 
     // 티켓 발급
-    function mintTicket(address _addr, uint i) public onlyOwner notCanceled(i){
+    function mintTicket(address _addr) public onlyOwner {
         uint tokenId = totalSupply() + 1;
         _mint(_addr, tokenId);
     }
--- frontend/src/abis/contractAddress.js
+++ frontend/src/abis/contractAddress.js
@@ -1,2 +1,2 @@
-export const PRE_EVENT_CONTRACT = "0xE6BC82A756dd2B5ff5c1dF0B0f69FfE60E5cb37c";
+export const PRE_EVENT_CONTRACT = "0x63fE1dfFbf112eFD8b4d2B022bE319fd9dd7C3CE";
 export const POST_EVENT_CONTRACT = "_contract address";
