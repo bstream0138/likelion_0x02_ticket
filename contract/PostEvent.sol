d68f0af2e32359701a9cc93149670bbeb653c799
Sepang2 <sepang296@gmail.com>
2024-02-26 00:26:41 +0900

    Modify functions and comments

--- contract/PostEvent.sol
+++ contract/PostEvent.sol
@@ -1,50 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-/*
-[Post-Event Ticket : 티켓의 굿즈화] - 소비자간의 소통, 우리는 소통 도우미
-
-<주최자>
-1. Post-Event 때 필요한 다양한 정보 요청
-    - 특정 굿즈 거래 횟수, 거래 가격 등등(인기 굿즈 선별)
-
-<소비자>
-1. 티켓 수집
-2. 2차 거래
-3. 등등
-
-<우리>
-1. 소비자끼리 굿즈화된 티켓들 잘 거래 할 수 있도록 함수 혹은 플랫폼 구현
-2. 주최자가 필요한 정보 제공(함수 혹은 web3 명령어 활용)
-*/
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PostTicket is ERC721Enumerable {
-    string beforeURI; // ex) 감사 이미지, 가수 셀카 등등
-    string afterURI; // ex) 굿즈 이미지 등, 예정된 팬미팅 포스터 등등 
-    bool isRevealed;
+// collection 역할의 Post-Ticket
+contract PostTicket is ERC721Enumerable, Ownable {
+    string musician;
+    string metadataURI; 
 
-    constructor(string memory _name, string memory _symbol, string memory _beforeURI, string memory _afterURI) ERC721(_name, _symbol) {
-        beforeURI = _beforeURI;
-        afterURI = _afterURI;
+    constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) Ownable(msg.sender) {
+        musician = _name;
+        metadataURI = _metadataURI;
     }
 
-    // 티켓 발급
-    function mintTicket(address _addr) public {
-        uint tokenId = totalSupply() + 1;
-        _mint(_addr, tokenId);
+    // 티켓 발급, 가스비 사용하므로 admin만 실행 가능
+    function mintTicket(address _addr, uint _tokenId) public onlyOwner {
+        _mint(_addr, _tokenId);
     }
-
-    function setStatus() public {
-        isRevealed = !isRevealed;
-    }
-
+ 
+    // 조건 추가하여 common, rare, unique 등급으로 나눔, 나중에 진짜 랜덤 조건으로 수정 필요
     function tokenURI(uint _tokenId) public view override returns(string memory) {
-        if(!isRevealed) {
-            return string(abi.encodePacked(beforeURI));
+        if(_tokenId % 6 == 0) {
+            return string(abi.encodePacked(metadataURI, '/', musician, '_Unique.json'));
+        } else if(_tokenId % 6 == 3 || _tokenId % 6 == 5) {
+            return string(abi.encodePacked(metadataURI, '/', musician, '_Rare.json'));
         } else {
-            return string(abi.encodePacked(afterURI));
+            return string(abi.encodePacked(metadataURI, '/', musician, '_Common.json'));
         }
     }
 }
